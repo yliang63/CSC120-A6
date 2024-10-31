591cfffc2254ee94f17f5d2e6379a49e9bbd9d05
yliang <yliang6@smith.edu>
2024-10-31 08:43:19 +0800

    javadoc.comment

--- Building.java
+++ Building.java
@@ -1,9 +1,20 @@
+/**
+ * This class represents a generic building with a name, address, and number of floors. It is the parents class for Cafe, Library and House.
+ */
 public class Building {
 
     protected String name = "<Name Unknown>";
     protected String address = "<Address Unknown>";
     protected int nFloors = 1;
 
+    /**
+     * Constructs a Cafe object with name, address, number of floors.
+     *
+     * @param name Name of the building
+     * @param address Address of the building
+     * @param nFloors Number of floors in the building
+
+     */
     public Building(String name, String address, int nFloors) {
         if (name != null) { this.name = name; }
         if (address != null) { this.address = address; } 
@@ -13,27 +24,50 @@ public class Building {
         this.nFloors = nFloors;
     }
 
+    /**
+     * Get the name of the building.
+     *
+     * @return Return the name of the building.
+     */
     public String getName() {
         return this.name;
     }
 
+    /**
+     * Get the address of the building.
+     *
+     * @return Return the address of the building.
+     */
     public String getAddress() {
         return this.address;
     }
 
+    /**
+     * Get the floor of the building.
+     *
+     * @return Return the floor of the building.
+     */
     public int getFloors() {
         return this.nFloors;
     }
 
+    /**
+     * Returns a string representation of the building.
+     *
+     * @return A string containing the name, number of floors, and address of the building
+     */
     public String toString() {
         return this.name + " is a " + this.nFloors + "-story building located at " + this.address;
     }
 
+    /**
+     * Main method to show the functionality of the Building class.
+     */
     public static void main(String[] args) {
         Building fordHall = new Building("Ford Hall", "100 Green Street Northampton, MA 01063", 4);
         System.out.println(fordHall);
     }
 
-    
+
 
 }
--- Cafe.java
+++ Cafe.java
@@ -5,7 +5,18 @@ public class Cafe extends Building {
     private int nSugarPackets;
     private int nCreams;
     private int nCups;
-    
+
+    /**
+     * Constructs a Cafe object with the provided name, address, number of floors, and initial inventory.
+     *
+     * @param name Name of the cafe
+     * @param address Address of the cafe
+     * @param nFloors Number of floors in the cafe
+     * @param nCoffeeOunces Quantity of coffee ounces in inventory
+     * @param nSugarPackets Quantity of sugar packets in inventory
+     * @param nCreams Quantity of cream packets in inventory
+     * @param nCups Quantity of cups in inventory
+     */
     public Cafe(String name, String address, int nFloors,int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
         super(name, address, nFloors);
         this.nCoffeeOunces = nCoffeeOunces;
@@ -13,7 +24,14 @@ public class Cafe extends Building {
         this.nCreams = nCreams;
         this.nCups = nCups;
   }
-    
+
+    /**
+     * Sells a coffee with their size of coffee, sugarpackets and creams that it needs.
+     *If the stuff needed is available, it will make a coffee. If they are not available, it will restock all the ingredients.
+     * @param size size of the coffee in ounces.
+     * @param nSugarPackets number of sugarpackets the drink requires.
+     * @param nCreams Number of creams the drink requires.
+     */
     public void sellCoffee(int size, int nSugarPackets, int nCreams) {
         if (size <= nCoffeeOunces && nSugarPackets <= this.nSugarPackets && nCreams <= this.nCreams && nCups > 0) {
             this.nCoffeeOunces -= size;
@@ -27,6 +45,13 @@ public class Cafe extends Building {
         }
     }
 
+    /**
+     * Restocked the ingredients when called.
+     * @param size size of the coffee in ounces.
+     * @param nSugarPackets number of sugarpackets that need to restock.
+     * @param nCreams Number of creams the drink that need to restock.
+     * @param nCups Number of cups that need to restock.
+     */
     private void restock(int nCoffeeOunces, int nSugarPackets, int nCreams, int nCups) {
         this.nCoffeeOunces = nCoffeeOunces;
         this.nSugarPackets = nSugarPackets;
@@ -35,6 +60,9 @@ public class Cafe extends Building {
         System.out.println("Inventory has been restocked.");
     }
 
+    /**
+     * Main method to show the functionality of the Cafe class.
+     */
     public static void main(String[] args) {
         System.out.println("You have built a cafe: ☕");
         Cafe cafe = new Cafe("Cafe", "123 Main St", 3, 20, 20, 40, 50);
